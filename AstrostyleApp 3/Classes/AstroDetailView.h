94c197c070e05f3deb029514317690e550102ddd
Shraddha Deshmukh <shraddha_sipl@systematixindia.com>
2011-05-04 16:17:19 +0530

    Latest Build 4/5/2011 4PM

---
+++ AstrostyleApp 3/Classes/AstroDetailView.h
@@ -0,0 +1,125 @@
+//
+//  AstroDetailView.h
+//  AstrostyleApp
+//
+//  Created by SIPL MacMini on 25/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FBDialog.h"
+#import "FBConnect.h"
+#import "FBSession.h"
+#import "FBRequest.h"
+#import "FBDialog.h"
+#import "FBStreamDialog.h"
+#import "FBLoginButton.h"
+#import "FBLoginDialog.h"
+#import "FBConnectGlobal.h"
+#import <MessageUI/MessageUI.h>
+#import <MessageUI/MFMailComposeViewController.h>
+
+@interface AstroDetailView : UIViewController <FBDialogDelegate,FBSessionDelegate,FBRequestDelegate,FBTranslationsLoaderDelegate,MFMailComposeViewControllerDelegate>{
+
+	
+		
+	IBOutlet UITextView *txtView;
+	NSString *bmonth;
+	NSString *bdate;
+	NSMutableDictionary *plistDictionary;
+	IBOutlet UIScrollView *scrollView;
+	IBOutlet UILabel *lblLeft;
+	IBOutlet UILabel *lblRight;
+	IBOutlet UILabel *lblZodiac;
+	IBOutlet UILabel *lblText;
+	IBOutlet UILabel *lblTitle;
+	IBOutlet UIView *whiteBG;
+	IBOutlet UIImageView *barimage;
+	
+	NSString *horoContent;
+	
+	NSXMLParser * rssParser;
+	
+	NSMutableArray * stories;
+		
+	NSMutableDictionary * item;
+	
+	
+	NSString * currentElement;
+	NSMutableString * currentTitle, * currentDate, * currentSummary, * currentLink;
+	UIActivityIndicatorView * activityIndicator;
+	NSString *todaysHoro;
+	NSString *weeksHoro;
+	NSString *monthsHoro;
+	NSString *zodiacSign;
+	NSString *bgImageName;
+	NSString *tileImageName;
+	IBOutlet UIImageView *bgImage;
+	IBOutlet UIImageView *tileImage;
+	
+	IBOutlet UIButton* _permissionButton;
+	IBOutlet UIButton* _feedButton;
+	IBOutlet UIButton* _statusButton;
+	IBOutlet UIButton* _photoButton;
+	IBOutlet FBLoginButton* _loginButton;
+	FBSession* _session;
+	
+	NSString *toEmailAddress;
+	NSString *errorMessage;
+	IBOutlet UILabel *message;
+	IBOutlet UIButton *emailBtn;
+	
+													
+}
+//@property (nonatomic,retain)Facebook *facebook;
+@property (nonatomic,retain)IBOutlet UILabel *lblTitle;
+@property (nonatomic,retain)IBOutlet UILabel *lblText;
+@property (nonatomic,retain)IBOutlet UILabel *lblLeft;
+@property (nonatomic,retain)IBOutlet UILabel *lblRight;
+@property (nonatomic,retain)NSString *bgImageName;
+@property (nonatomic,retain)NSString *tileImageName;
+@property (nonatomic,retain)UIImageView *bgImage;
+@property (nonatomic,retain)UIImageView *tileImage;
+@property (nonatomic,retain)NSString *zodiacSign;
+@property (nonatomic,retain)NSString *todaysHoro;
+@property (nonatomic,retain)NSString *weeksHoro;
+@property (nonatomic,retain)NSString *monthsHoro;
+@property (nonatomic,retain)IBOutlet UITextView *txtView;
+@property (nonatomic,retain)NSString *bmonth;
+@property (nonatomic,retain)NSString *bdate;
+@property (nonatomic,retain)NSMutableDictionary *plistDictionary;
+@property (nonatomic,retain)IBOutlet UIScrollView *scrollView;
+@property (nonatomic,retain)NSString *horoContent;
+@property (nonatomic,retain)IBOutlet UILabel *lblZodiac;
+
+@property(nonatomic,retain) NSString *toEmailAddress;
+@property(nonatomic,retain) NSString *bccEmailAddress;
+@property(nonatomic,retain) NSString *errorMessage;
+
+
+-(void)getTodaysHoroscope;
+-(void)getWeekHoroscope;
+-(void)getMonthsHoroscope;
+- (IBAction)displayWeeklyHoroscope;
+- (IBAction)displayTodaysHoroscope;
+- (IBAction)displayMonthlyHoroscope;
+- (IBAction)frontSwipeDetected;
+- (IBAction)backSwipeDetected;
+
+//Facebook Methods
+- (IBAction)askPermission;
+- (IBAction)publishStatus;
+- (IBAction)publishFeed:(id)target;
+- (IBAction)setStatus:(id)target;
+- (IBAction)uploadPhoto:(id)target;
+- (void)translationExamples;
+- (void)translationsDidLoad;
+- (void)translationsDidFailWithError:(NSError *)error;
+
+
+-(IBAction)emailHoroscope;
+-(void)displayComposerSheet;
+-(void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error; 
+-(void)launchMailAppOnDevice;
+
+@end
---
+++ AstrostyleApp 3/singbox-facebook-iphone-sdk-ba72051 3/samples/Connect/SessionViewController.m
@@ -0,0 +1,249 @@
+/*
+ * Copyright 2009-2010 Facebook
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+#import "SessionViewController.h"
+#import "FBConnect/FBConnect.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// This application will not work until you enter your Facebook application's API key here:
+
+static NSString* kApiKey = @"128635573869073";
+
+// Enter either your API secret or a callback URL (as described in documentation):
+static NSString* kApiSecret = @"b34f22c35fe557d7f56aa79c71e065cf"; // @"<YOUR SECRET KEY>";
+static NSString* kGetSessionProxy = nil; // @"<YOUR SESSION CALLBACK)>";
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+@implementation SessionViewController
+
+@synthesize label = _label;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// NSObject
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+  if (self = [super initWithNibName:@"SessionViewController" bundle:nibBundleOrNil]) {
+    if (kGetSessionProxy) {
+      _session = [[FBSession sessionForApplication:kApiKey getSessionProxy:kGetSessionProxy
+                             delegate:self] retain];
+    } else {
+      _session = [[FBSession sessionForApplication:kApiKey secret:kApiSecret delegate:self] retain];
+    }
+  }
+  return self;
+}
+
+- (void)dealloc {
+  [_session release];
+  [super dealloc];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// UIViewController
+
+- (void)viewDidLoad {
+  [_session resume];
+  _loginButton.style = FBLoginButtonStyleWide;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+  return YES;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// FBDialogDelegate
+
+- (void)dialog:(FBDialog*)dialog didFailWithError:(NSError*)error {
+  _label.text = [NSString stringWithFormat:@"Error(%d) %@", error.code,
+    error.localizedDescription];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// FBSessionDelegate
+
+- (void)session:(FBSession*)session didLogin:(FBUID)uid {
+  _label.text = @"";
+  _permissionButton.hidden = NO;
+  _feedButton.hidden       = NO;
+  _statusButton.hidden     = NO;
+  _photoButton.hidden      = NO;
+
+  NSString* fql = [NSString stringWithFormat:
+    @"select uid,name from user where uid == %lld", session.uid];
+
+  NSDictionary* params = [NSDictionary dictionaryWithObject:fql forKey:@"query"];
+  [[FBRequest requestWithDelegate:self] call:@"facebook.fql.query" params:params];
+  
+  [self translationExamples];
+}
+
+- (void)sessionDidNotLogin:(FBSession*)session {
+  _label.text = @"Canceled login";
+}
+
+- (void)sessionDidLogout:(FBSession*)session {
+  _label.text = @"Disconnected";
+  _permissionButton.hidden = NO;
+  _feedButton.hidden       = NO;
+  _statusButton.hidden     = NO;
+  _photoButton.hidden      = NO;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// FBRequestDelegate
+
+- (void)request:(FBRequest*)request didLoad:(id)result {
+  if ([request.method isEqualToString:@"facebook.fql.query"]) {
+    NSArray* users = result;
+    NSDictionary* user = [users objectAtIndex:0];
+    NSString* name = [user objectForKey:@"name"];
+    _label.text = [NSString stringWithFormat:@"Logged in as %@", name];
+  } else if ([request.method isEqualToString:@"facebook.users.setStatus"]) {
+    NSString* success = result;
+    if ([success isEqualToString:@"1"]) {
+      _label.text = [NSString stringWithFormat:@"Status successfully set"]; 
+    } else {
+      _label.text = [NSString stringWithFormat:@"Problem setting status"]; 
+    }
+  } else if ([request.method isEqualToString:@"facebook.photos.upload"]) {
+    NSDictionary* photoInfo = result;
+    NSString* pid = [photoInfo objectForKey:@"pid"];
+    _label.text = [NSString stringWithFormat:@"Uploaded with pid %@", pid];
+  }
+}
+
+- (void)request:(FBRequest*)request didFailWithError:(NSError*)error {
+  _label.text = [NSString stringWithFormat:@"Error(%d) %@", error.code,
+    error.localizedDescription];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+- (void)askPermission:(id)target {
+  FBPermissionDialog* dialog = [[[FBPermissionDialog alloc] init] autorelease];
+  dialog.delegate = self;
+  dialog.permission = @"status_update";
+  [dialog show];
+}
+
+- (void)publishFeed:(id)target {
+  FBStreamDialog* dialog = [[[FBStreamDialog alloc] init] autorelease];
+  dialog.delegate = self;
+  dialog.userMessagePrompt = @"Example prompt";
+  dialog.attachment = @"{\"name\":\"Facebook Connect for iPhone\",\"href\":\"http://developers.facebook.com/connect.php?tab=iphone\",\"caption\":\"Caption\",\"description\":\"Description\",\"media\":[{\"type\":\"image\",\"src\":\"http://img40.yfrog.com/img40/5914/iphoneconnectbtn.jpg\",\"href\":\"http://developers.facebook.com/connect.php?tab=iphone/\"}],\"properties\":{\"another link\":{\"text\":\"Facebook home page\",\"href\":\"http://www.facebook.com\"}}}";
+  // replace this with a friend's UID
+  // dialog.targetId = @"999999";
+  [dialog show];
+}
+
+- (void)setStatus:(id)target {
+  NSString *statusString = @"Testing iPhone Connect SDK";
+  NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys:
+              statusString, @"status",
+              @"true", @"status_includes_verb",
+              nil];
+  [[FBRequest requestWithDelegate:self] call:@"facebook.users.setStatus" params:params];
+}
+
+- (void)uploadPhoto:(id)target {
+  NSString *path = @"http://www.facebook.com/images/devsite/iphone_connect_btn.jpg";
+  NSURL    *url  = [NSURL URLWithString:path];
+  NSData   *data = [NSData dataWithContentsOfURL:url];
+  UIImage  *img  = [[UIImage alloc] initWithData:data];
+  
+  NSDictionary *params = nil;
+  [[FBRequest requestWithDelegate:self] call:@"facebook.photos.upload" params:params dataParam:(NSData*)img];
+}
+
+// FB Translation Framework examples
+
+- (void)uploadSomeStrings {
+  NSError *error = nil;
+  int result;
+
+  NSString *newString =
+    [NSString stringWithFormat:@"String to translate from iPhone SDK, %d.",
+     rand() % 10000];
+  NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithCapacity:10];
+  [dict setObject:@"comment" forKey:newString];
+
+  result = [FBNativeStringUploader uploadStringSet:dict error:&error];
+}
+
+/*
+ * A list of supported locales can be found at:
+ * http://www.facebook.com/translations/AppleToFbLocales.plist
+ * Or for a programmatic check, see [FBTranslationsLoader supportsLocale:].
+ */
+- (void)getSomeTranslations {
+  NSString *failure = @"Supported locale assertion failed";
+  NSAssert2([FBTranslationsLoader supportsLocale:@"es_ES"] == 1,
+            failure,
+            1,
+            [FBTranslationsLoader supportsLocale:@"es_ES"]);
+  
+  NSAssert2([FBTranslationsLoader supportsLocale:@"xx_YY"] == 0,
+            failure,
+            0,
+            [FBTranslationsLoader supportsLocale:@"xx_YY"]);
+  
+  
+  [FBTranslationsLoader loadTranslationsForLocale:@"es_ES" delegate:self];
+}
+
+- (void)translationExamples {
+  [self uploadSomeStrings];
+  
+  [self getSomeTranslations];
+}
+
+- (void)assertExpectedTranslation:(NSString *)nativeString
+                      description:(NSString *)description
+              expectedTranslation:(NSString *)expectedTranslation {
+  NSString *translationMismatch =
+  @"Translations mismatch. Expected <%@>, got <%@>.";
+
+  NSAssert2(
+            [FBLocalizedString(nativeString, description) 
+             isEqualToString:expectedTranslation],
+            translationMismatch,
+            expectedTranslation,
+            FBLocalizedString(nativeString, description)
+            );  
+}
+
+- (void)translationsDidLoad {
+  NSString *dummy = FBLocalizedString(@"Test String 6", @"Sample description");
+  NSString *dummy2 =
+    FBLocalizedString(@"Test String 5", @"Test of \"quotes.\", \n, \t.");
+  
+ // [self assertExpectedTranslation:@"Test String 6"
+ //                     description:@"Sample description"
+ //             expectedTranslation:@"Sample translation"];
+  
+ // [self assertExpectedTranslation:@"Test String 5"
+ //                     description:@"Test of \"quotes.\", \n, \t."
+ //             expectedTranslation:@"Test of \"quotes.\", \n, \t."];
+
+}
+
+- (void)translationsDidFailWithError:(NSError *)error {
+  NSAssert(false, @"Loading translations errored.");
+}
+
+
+@end
